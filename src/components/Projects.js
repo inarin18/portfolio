e155305438ccc993166b66368f3b7b7742465d0a
Hinata <69310436+inarin18@users.noreply.github.com>
2024-10-21 21:12:21 +0900

    add Signate, Elastic Deformation

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,22 +1,22 @@
 const projects = [
     {
-        title: "Sample Project 1",
-        description: "Developed a state-of-the-art neural network architecture for image recognition, achieving 98% accuracy on benchmark datasets.",
-        date: "2023",
+        title: "Sigante Competition - Silver Medal",
+        description: "Developed a predictive model for banking app review scores using quantitative and qualitative data from user reviews and 'like' counts. Implemented an LLM to handle natural language variables. Then I got a silver medal in the Sigante competition.",
+        date: "2024/08-09",
         link: "#",
-        technologies: ["Python", "TensorFlow", "OpenCV"]
+        technologies: ["Python", "PyTorch", "Transformers", "Scikit-learn", "Pandas", "Numpy", "LLM"]
     },
     {
-        title: "Sample Project 2",
-        description: "Created an NLP system capable of understanding and generating human-like text, with applications in chatbots and content generation.",
-        date: "2022",
+        title: "Implemented Elastic Deformation from Scratch",
+        description: "Developed a custom implementation of Elastic Deformation, an advanced data augmentation technique. This algorithm distorts images while preserving their overall structure, generating new training samples. The method enhances model robustness and generalization in image recognition tasks.",
+        date: "2023/07",
         link: "#",
-        technologies: ["Python", "PyTorch", "NLTK", "Transformers"]
+        technologies: ["Python", "NumPy", "Pillow", "Image Processing"]
     },
     {
         title: "Sample Project 3",
         description: "Implemented a deep reinforcement learning algorithm to teach robots complex manipulation tasks in simulated environments.",
-        date: "2021",
+        date: "202",
         link: "#",
         technologies: ["Python", "TensorFlow", "OpenAI Gym"]
     },
