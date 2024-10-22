43fec51b1e4687733b8f19a5b9f21d3463ba968c
Hinata <69310436+inarin18@users.noreply.github.com>
2024-10-23 06:50:05 +0900

    言語切り替えに対応

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
-import PortfolioMain from './main';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
-import './i18n';
+import PortfolioMain from './main';
+import ProjectDetail from './components/ProjectDetail';
+import LanguageSwitcher from './components/LanguageSwitcher';
 
 function App() {
+
+  const [language, setLanguage] = useState('en');
+
   return (
-    <div className="App">
-      <PortfolioMain />
-    </div>
+    <Router>
+      <div className="App">
+        <LanguageSwitcher setLanguage={setLanguage} currentLanguage={language}/>
+        <Routes>
+            <Route path="/" element={<Navigate to={`/${language}`} />} />
+            <Route path="/:lang" element={<PortfolioMain language={language} />} />
+            <Route path="/:lang/project/:id" element={<ProjectDetail language={language} />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
