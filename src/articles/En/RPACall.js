50e61b64aae2e0ca6d406589645f12fe8903a742
Hinata <69310436+inarin18@users.noreply.github.com>
2024-10-23 07:21:52 +0900

    first commit

---
+++ src/articles/En/RPACall.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RPACall = () => (
+    <article>
+        <h3>Key Features</h3>
+    </article>
+);
+
+export default RPACall;
---
+++ src/articles/Ja/RPACall.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RPACall = () => (
+    <article>
+        <h3>主な成果</h3>
+    </article>
+);
+
+export default RPACall;
